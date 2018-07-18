d9fdae3792cf4e76dcde4bb097535e44b9031987
Markiian Yuskevych <markiyan.yuskevych@gmail.com>
2018-07-18 03:11:07 +0300

    Title
    Body
    
    PEGS-0

--- dags/aws_glue_example.py
+++ dags/aws_glue_example.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.aws_glue_operator import AWSGlueJobOperator
+
 def print_hello():
     return 'Hello hello!'
 
@@ -10,10 +10,15 @@ def print_dummy():
     return 'Hello dummy!'
 
 dag = DAG('hello_world', description='Simple glue DAG',
-          schedule_interval='0 0 * * *',
+          schedule_interval='@hourly',
           start_date=datetime(2018, 6, 28), catchup=False)
 
-awsGlueOperator = AWSGlueJobOperator(job_name='FIRST_JOB', script_location=https://s3.us-east-2.amazonaws.com/aws-glue-scripts-001264113720-us-east-2/root/FIRST_JOB,region_name=us-east-2,s3_bucket=https://s3.console.aws.amazon.com/s3/, iam_role_name=AWSGlueServiceRole-Craw, dag=dag)
+awsGlueOperator = AWSGlueJobOperator(job_name='FIRST_JOB',
+script_location='https://s3.us-east-2.amazonaws.com/aws-glue-scripts-001264113720-us-east-2/root/FIRST_JOB',
+region_name='us-east-2',
+s3_bucket='https://s3.console.aws.amazon.com/s3/', 
+iam_role_name='AWSGlueServiceRole-Craw', 
+dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
--- plugins/AWSGlueJobSensor.py
+++ plugins/AWSGlueJobSensor.py
@@ -1,25 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from airflow.operators.sensors import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
-from airflow.contrib.hooks.aws_glue_job_hook import AwsGlueJobHook
+
+from AWSGlueJobHook import AwsGlueJobHook
 
 
 class AwsGlueJobSensor(BaseSensorOperator):
@@ -53,4 +35,4 @@ class AwsGlueJobSensor(BaseSensorOperator):
         hook = AwsGlueJobHook(aws_conn_id=self.aws_conn_id)
         job_state = hook.job_completion(job_name=self.job_name,
                                         run_id=self.run_id)
-return job_state.upper() in self.targeted_status
+        return job_state.upper() in self.targeted_status
